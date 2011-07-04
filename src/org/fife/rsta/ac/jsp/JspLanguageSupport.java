c3abddb6f4b917a8e229749b93159281f12527dc
bobbylight <robert@fifesoft.com>
2011-07-04 14:18:04 +0000

    Beginning JSP language support.

---
+++ src/org/fife/rsta/ac/jsp/JspLanguageSupport.java
@@ -0,0 +1,36 @@
+package org.fife.rsta.ac.jsp;
+
+import org.fife.rsta.ac.AbstractLanguageSupport;
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
+
+
+/**
+ * Language support for JSP.
+ *
+ * @author Robert Futrell
+ * @version 1.0
+ */
+public class JspLanguageSupport extends AbstractLanguageSupport {
+
+
+	/**
+	 * Constructor.
+	 */
+	public JspLanguageSupport() {
+		
+	}
+
+
+	public void install(RSyntaxTextArea textArea) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	public void uninstall(RSyntaxTextArea textArea) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+}
