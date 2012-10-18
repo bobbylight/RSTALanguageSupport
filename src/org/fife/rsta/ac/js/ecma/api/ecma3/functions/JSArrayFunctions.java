07ab25b5519220f132fff78e294bf77641d9a51f
Steve Upton <steve@upton.me.uk>
2012-10-18 16:46:09 +0000

    Added some javadoc for Array

--- src/org/fife/rsta/ac/js/ecma/api/ecma3/functions/JSArrayFunctions.java
+++ src/org/fife/rsta/ac/js/ecma/api/ecma3/functions/JSArrayFunctions.java
@@ -4,14 +4,23 @@ import org.fife.rsta.ac.js.ecma.api.ecma3.JSArray;
 import org.fife.rsta.ac.js.ecma.api.ecma3.JSFunction;
 import org.fife.rsta.ac.js.ecma.api.ecma3.JSNumber;
 import org.fife.rsta.ac.js.ecma.api.ecma3.JSObject;
+import org.fife.rsta.ac.js.ecma.api.ecma3.JSString;
 
 
 public interface JSArrayFunctions extends JSObjectFunctions {
 	/**
-	 * function concat(args)
+	 * <b>function concat(args)</b> creates and returns a new <b>array</b> that is the result of concatenating each
+	 * of its arguments to an array.
+	 * <h3>Example</h3>
+	 * <code>
+	 * 	var c = [1,2,3];<br>
+	 * 	c.concat(4,5); //Returns [1,2,3,4,5]<br>
+	 *  c.concat([4,5]); //Returns [1,2,3,4,5]<br>
+	 *  c.concat(4,[5,[6, 7]]); //Returns [1,2,3,4,5, [6,7]]<br>
+	 * </code> 
 	 * 
-	 * @param {Array} args
-	 * @returns {Array}
+	 * @param {Array} args ...Any number of values to be concatenated to an <b>array</b>
+	 * @returns {Array} A new <b>array</b>
 	 * @memberOf Array
 	 * @see org.fife.rsta.ac.js.ecma.api.ecma3.JSArray Array
 	 * @since Standard ECMA-262 3rd. Edition
@@ -21,24 +30,37 @@ public interface JSArrayFunctions extends JSObjectFunctions {
 
 
 	/**
-	 * function join(seperator)
+	 * <b>function join(separator)</b> converts each element of the <b>array</b> into a string. Uses the separator between the elements. 
+	 * <h3>Example</h3>
+	 * <code>
+	 * 	var a = [1,2,3];<br>
+	 * 	var s = a.join("|"); //s is the string "1|2|3"<br>
+	 *  
+	 * </code> 
 	 * 
-	 * @param {String} seperator
-	 * @returns {Array}
+	 * @param {String} <i>separator</i> An optional character or string used to separate each element with the string.  
+	 * @returns {String} A string representing the result of all the elements in the <b>array</b> concatenated together, separated by the separator. 
 	 * @memberOf Array
 	 * @see org.fife.rsta.ac.js.ecma.api.ecma3.JSArray Array
 	 * @since Standard ECMA-262 3rd. Edition
 	 * @since Level 2 Document Object Model Core Definition.
 	 */
-	public JSArray join(String seperator);
+	public JSString join(String seperator);
 
 
 	/**
-	 * function pop()
-	 * 
-	 * @returns {Object}
+	 * <b>function pop()</b> removes and returns the last element of an <b>array</b>.
+	 * <h3>Example</h3>
+	 * <code>
+	 * 	var stack = [1,2,3];<br>
+	 * 	stack.pop(); // returns 3, stack [1, 2]<br>
+	 * 	stack.pop(); // returns 2,  stack [1]<br>
+	 *  stack.pop(); // returns 1,  stack []<br>
+	 * </code> 
+	 * @returns {Object} The last element of the <b>array</b>
 	 * @memberOf Array
 	 * @see org.fife.rsta.ac.js.ecma.api.ecma3.JSArray Array
+	 * @see #push(JSArray) push()
 	 * @since Standard ECMA-262 3rd. Edition
 	 * @since Level 2 Document Object Model Core Definition.
 	 */
@@ -46,11 +68,16 @@ public interface JSArrayFunctions extends JSObjectFunctions {
 
 
 	/**
-	 * function push(args)
-	 * 
-	 * @param {Array} args
+	 * <b>function push(args)</b> append elements to an <b>array</b>.
+	 * <h3>Example</h3>
+	 * <code>
+	 * 	var vals = [];<br>
+	 * 	vals.push(1,2,3); // returns new array [1,2,3]<br>
+	 * </code> 
+	 * @param {Array} args One or more values to be appended to the end of the <b>array</b>
 	 * @memberOf Array
 	 * @see org.fife.rsta.ac.js.ecma.api.ecma3.JSArray Array
+	 * @see #pop()
 	 * @since Standard ECMA-262 3rd. Edition
 	 * @since Level 2 Document Object Model Core Definition.
 	 */
@@ -58,9 +85,13 @@ public interface JSArrayFunctions extends JSObjectFunctions {
 
 
 	/**
-	 * function reverse()
-	 * 
-	 * @returns {Array}
+	 * <b>function reverse()</b> reverse the elements of an <b>array</b>
+	 * <h3>Example</h3>
+	 * <code>
+	 * 	var r = [1,2,3];<br>
+	 * 	r.reverse(); // r is now [3,2,1]<br>
+	 * </code> 
+	 * @returns {Array} The <b>array</b> after it has been reversed
 	 * @memberOf Array
 	 * @see org.fife.rsta.ac.js.ecma.api.ecma3.JSArray Array
 	 * @since Standard ECMA-262 3rd. Edition
@@ -70,11 +101,18 @@ public interface JSArrayFunctions extends JSObjectFunctions {
 
 
 	/**
-	 * function shift()
-	 * 
-	 * @returns {Object}
+	 * <b>function shift()</b> shift <b>array</b> elements down
+	 * <h3>Example</h3>
+	 * <code>
+	 * 	var s = [1,2,3];<br>
+	 * 	s.shift(); // Returns 1; s = [2,3]<br>
+	 *  s.shift(); // Returns 2; s = [3]<br>
+	 * </code> 
+	 * @returns {Object} The former first element of the <b>array</b>
 	 * @memberOf Array
 	 * @see org.fife.rsta.ac.js.ecma.api.ecma3.JSArray Array
+	 * @see #pop() pop()
+	 * @see #unshift(JSArray) unshift()
 	 * @since Standard ECMA-262 3rd. Edition
 	 * @since Level 2 Document Object Model Core Definition.
 	 */
@@ -82,13 +120,20 @@ public interface JSArrayFunctions extends JSObjectFunctions {
 
 
 	/**
-	 * function slice(start, end)
-	 * 
-	 * @param {Number} start
-	 * @param {Number} end
-	 * @returns {Array}
+	 * <b>function slice(start, end)</b> return a portion of an <b>array</b>
+	 * <h3>Example</h3>
+	 * <code>
+	 * 	var s = [1,2,3,4,5];<br>
+	 * 	s.slice(0,3); // Returns [1,2,3]<br>
+	 *  s.slice(3); // Returns [4,5]<br>
+	 *  s.slice(1,-1); // Returns [2,3,4]<br>
+	 * </code> 
+	 * @param {Number} start The array index from where to begin.  If negative, this argument specifies a position measured from the end of the array. 
+	 * @param {Number} end The array index immediately after the end of the slice. If not specified then the slice includes all the array elements from the start to the end of the array.
+	 * @returns {Array} A new <b>array</b> containing elements from the <b><i>start</b></i> up to, but not including the <b><i>end</i></b> of the slice.
 	 * @memberOf Array
 	 * @see org.fife.rsta.ac.js.ecma.api.ecma3.JSArray Array
+	 * @see #splice(JSNumber, JSNumber, JSArray) splice();
 	 * @since Standard ECMA-262 3rd. Edition
 	 * @since Level 2 Document Object Model Core Definition.
 	 */
@@ -96,10 +141,16 @@ public interface JSArrayFunctions extends JSObjectFunctions {
 
 
 	/**
-	 * function sort(function)
-	 * 
-	 * @param {Function} function
-	 * @returns {Array}
+	 * <b>function sort(function)</b> sort the elements of an <b>array</b>
+	 * <h3>Example</h3>
+	 * <code>
+	 *  function numbersort (a,b){return a-b;}<br>
+	 * 	var s = new Array(22,1111,33,4,55]);<br>
+	 * 	s.sort(); //Alphabetical order : 1111,22,33,4,55<br>
+	 *  s.sort(numbersort); //Numerical order : 4, 22, 33, 55, 1111<br>
+	 * </code> 
+	 * @param {Function} <i>function</i> an optional function used to specify the sorting order
+	 * @returns {Array} A reference to the <b>array</b>
 	 * @memberOf Array
 	 * @see org.fife.rsta.ac.js.ecma.api.ecma3.JSArray Array
 	 * @since Standard ECMA-262 3rd. Edition
@@ -109,14 +160,21 @@ public interface JSArrayFunctions extends JSObjectFunctions {
 
 
 	/**
-	 * function splice(start, deletecount, items)
-	 * 
-	 * @param {Number} start
-	 * @param {Number} deletecount
-	 * @param {Array} items
-	 * @returns {Array}
+	 * <b>function splice(start, deletecount, items)</b> insert, remove, or replace <b>array</b> elements
+	 * <h3>Example</h3>
+	 * <code>
+	 *  var s = [1,2,3,4,5,6,7,8];<br>
+	 * 	s.splice(1,2); //Returns [2,3]; s is [1,4]<br>
+	 *  s.splice(1,1); //Returns [4]; s is [1]<br>
+	 *  s.splice(1,0,2,3); //Returns []; s is [1 2 3]<br>
+	 * </code> 
+	 * @param {Number} start the <b>array</b> element at which the insertion and/or deletion is to begin
+	 * @param {Number} <i>deletecount</i> The number of elements starting with and including <i><b>start</b></i>. 
+	 * @param {Array} items zero or more items to be inserted into the <b>array</b>
+	 * @returns {Array} An <b>array</b> containing the elements, if any, deleted from the <b>array</b>
 	 * @memberOf Array
 	 * @see org.fife.rsta.ac.js.ecma.api.ecma3.JSArray Array
+	 * @see #shift() shift();
 	 * @since Standard ECMA-262 3rd. Edition
 	 * @since Level 2 Document Object Model Core Definition.
 	 */
@@ -124,14 +182,21 @@ public interface JSArrayFunctions extends JSObjectFunctions {
 
 
 	/**
-	 * function unshift(items)
-	 * 
-	 * @param {Array} start
-	 * @returns {Array}
+	 * <b>function unshift(items)</b> insert elements at the beginning of an <b>array</b>
+	 * <h3>Example</h3>
+	 * <code>
+	 *  var s = [];<br>
+	 * 	s.unshift(1,2); //Returns 2; s is [1,2]<br>
+	 *  s.unshift(22); //Returns 3; s is [22,1,2]<br>
+	 *  s.shift(); //Returns 22; s is [1,2]<br>
+	 * </code> 
+	 * @param {Array} value One or more values to insert at the beginning of the <b>array</b>
+	 * @returns {Number} The new length of the array
 	 * @memberOf Array
 	 * @see org.fife.rsta.ac.js.ecma.api.ecma3.JSArray Array
+	 * @see #shift() shift()
 	 * @since Standard ECMA-262 3rd. Edition
 	 * @since Level 2 Document Object Model Core Definition.
 	 */
-	public JSArray unshift(JSArray start);
+	public JSNumber unshift(JSArray value);
 }
--- src/org/fife/rsta/ac/js/ecma/api/ecma5/functions/JS5ArrayFunctions.java
+++ src/org/fife/rsta/ac/js/ecma/api/ecma5/functions/JS5ArrayFunctions.java
@@ -12,36 +12,56 @@ import org.fife.rsta.ac.js.ecma.api.ecma5.JS5Object;
 public interface JS5ArrayFunctions extends JS5ObjectFunctions, JSArrayFunctions {
 	
 	/**
-	 * function every(predicate, o)
-	 * 
-	 * @param {Function} predicate
-	 * @param {Object} o
-	 * @returns {Boolean}
+	 * <b>function every(predicate, o)</b> test whether predicate is true for every element
+	 * <h3>Example</h3>
+	 * <code>
+	 *  [1,2,3].every(function(x){return x < 5;} //=>true<br>
+	 *  [1,2,3].every(function(x){return x < 2;} //=>false<br>
+	 * 	[].every(function(x){return false;} //=>true, always true for []<br>
+	 * </code> 
+	 * @param {Function} predicate A predicate function to test array elements
+	 * @param {Object} <i>o</i> The optional <b><i>this</i></b> value for invocations of <b><i>predicate</i></b>.
+	 * @returns {Boolean} <b><i>true</i></b> if <b><i>predicate</i></b> is true for every element of the <b>array</b> or <b><i>false</i></b>
 	 * @memberOf Array
 	 * @see org.fife.rsta.ac.js.ecma.api.ecma5.JS5Array Array
+	 * @see #filter(JS5Function, JS5Object) filter()
+	 * @see #forEach(JS5Function, JS5Object) forEach()
+	 * @see #some(JS5Function, JS5Object) some()
 	 * @since Standard ECMA-262 5th. Edition
 	 */
 	public JSBoolean every(JS5Function predicate, JS5Object o);
 	
 	
 	/**
-	 * function filter(predicate, o)
-	 * 
-	 * @param {Function} predicate
-	 * @param {Object} o
-	 * @returns {Array}
+	 * <b>function filter(predicate, o)</b> return array elements that pass a predicate
+	 * <h3>Example</h3>
+	 * <code>
+	 *  [1,2,3].filter(function(x){return x > 1}); // returns [2,3]<br>
+	 * </code> 
+	 * @param {Function} predicate The function to invoke to determine whether an element of <b>array</b> will be included in the returned array.
+	 * @param {Object} <i>o</i> An optional value on which <b><i>predicate</i></b> is invoked
+	 * @returns {Array} a new <b>array</b> containing only those elements of <b>array</b> for which <b><i>predicate</i></b> returned <b><i>true</i></b>
 	 * @memberOf Array
 	 * @see org.fife.rsta.ac.js.ecma.api.ecma5.JS5Array Array
+	 * @see #every(JS5Function, JS5Object) every()
+	 * @see #forEach(JS5Function, JS5Object) forEach()
+	 * @see #indexOf(JS5Object, JSNumber) indexOf()
+	 * @see #map(JS5Function, JS5Object) map()
+	 * @see #reduce(JS5Function, JS5Object) reduce()
 	 * @since Standard ECMA-262 5th. Edition
 	 */
 	public JS5Array filter(JS5Function predicate, JS5Object o);
 	
 	
 	/**
-	 * function forEach(f, o)
-	 * 
-	 * @param {Function} f
-	 * @param {Object} o
+	 * <b>function forEach(f, o)</b> invoke a function for each array element
+	 * <h3>Example</h3>
+	 * <code>
+	 *  var a = [1,2,3];<br>
+	 *  a.forEach(function(x,i,a){a[i]++;}); //a is now [2,3,4]<br>
+	 * </code> 
+	 * @param {Function} f The function to invoke for each element of <b>array</b>
+	 * @param {Object} <i>o</i> An optional value on which <b><i>f</b></i> is invoked 
 	 * @memberOf Array
 	 * @see org.fife.rsta.ac.js.ecma.api.ecma5.JS5Array Array
 	 * @since Standard ECMA-262 5th. Edition
@@ -50,75 +70,104 @@ public interface JS5ArrayFunctions extends JS5ObjectFunctions, JSArrayFunctions 
 	
 	
 	/**
-	 * function indexOf(value, start)
-	 * 
-	 * @param {Object} value
-	 * @param {Number} start
-	 * @returns {Number}
+	 * <b>function indexOf(value, start)</b> search an array
+	 * <h3>Example</h3>
+	 * <code>
+	 *  ['a','b','c'].indexOf('b'); //returns 1<br>
+	 *  ['a','b','c'].indexOf('d'); //returns -1<br>
+	 *  ['a','b','c'].indexOf('a',1); //returns -1<br>
+	 * </code> 
+	 * @param {Object} value The value to search <b>array</b> for.
+	 * @param {Number} <i>start</i> An optional array index at which to begin the search. If omitted, 0 is used.
+	 * @returns {Number} The <i>lowest</i> index => start of <b>array</b> at which the element matches <b><i>value</i></b>. Or -1 if no match is found
 	 * @memberOf Array
 	 * @see org.fife.rsta.ac.js.ecma.api.ecma5.JS5Array Array
+	 * @see #lastIndexOf(JS5Object, JSNumber) lastIndexOf()
 	 * @since Standard ECMA-262 5th. Edition
 	 */
 	public JSNumber indexOf(JS5Object value, JSNumber start);
 	
 	
 	/**
-	 * function lastIndexOf(value, start)
+	 * <b>function lastIndexOf(value, start)</b> search backwards through an array
 	 * 
-	 * @param {Object} value
-	 * @param {Number} start
-	 * @returns {Number}
+	 * @param {Object} value The value to search <b>array</b> for.
+	 * @param {Number} <i>start</i> An optional array index at which to begin the search. If omitted, the search begins at the last element 
+	 * @returns {Number} The <i>highest</i> index => start of <b>array</b> at which the element matches <b><i>value</i></b>. Or -1 if no match is found
 	 * @memberOf Array
 	 * @see org.fife.rsta.ac.js.ecma.api.ecma5.JS5Array Array
+	 * @see #indexOf(JS5Object, JSNumber) indexOf()
 	 * @since Standard ECMA-262 5th. Edition
 	 */
 	public JSNumber lastIndexOf(JS5Object value, JSNumber start);
 	
 	
 	/**
-	 * function map(f, o)
-	 * 
-	 * @param {Function} f
-	 * @param {Object} o
-	 * @returns {Array}
+	 * <b>function map(f, o)</b> compute new array elements from old
+	 * <h3>Example</h3>
+	 * <code>
+	 *  [1,2,3].map(function(x){return x*x;}); //returns [1,4,9]<br>
+	 * </code> 
+	 * @param {Function} f The function to invoke for each element of <b>array</b>. Its return becomes the elements of the returned array
+	 * @param {Object} <i>o</i> An optional value of which <b><i>f</i></b> is invoked 
+	 * @returns {Array} A new array with elements computed by function <b><i>f</i></b>
 	 * @memberOf Array
 	 * @see org.fife.rsta.ac.js.ecma.api.ecma5.JS5Array Array
+	 * @see #every(JS5Function, JS5Object) every()
+	 * @see #filter(JS5Function, JS5Object) filter()
+	 * @see #forEach(JS5Function, JS5Object) forEach()
+	 * @see #reduce(JS5Function, JS5Object) reduce()
 	 * @since Standard ECMA-262 5th. Edition
 	 */
 	public JS5Array map(JS5Function f, JS5Object o);
 	
 	/**
-	 * function reduce(f, initial)
-	 * 
-	 * @param {Function} f
-	 * @param {Object} initial
+	 * <b>function reduce(f, initial)</b> compute a value from the elements of an array
+	 * <h3>Example</h3>
+	 * <code>
+	 *  [1,2,3].reduce(function(x,y){return x*y;}); //returns 6 ((1*2(*3))<br>
+	 * </code> 
+	 * @param {Function} f A function that combines two values and returns a "reduced" value
+	 * @param {Object} <i>initial</i> An optional initial value to see the array reduction with. 
 	 * @returns {Object}
 	 * @memberOf Array
 	 * @see org.fife.rsta.ac.js.ecma.api.ecma5.JS5Array Array
+	 * @see #forEach(JS5Function, JS5Object) forEach()
+	 * @see #map(JS5Function, JS5Object) map()
+	 * @see #reduceRight(JS5Function, JS5Object) reduceRight()
 	 * @since Standard ECMA-262 5th. Edition
 	 */
 	public JS5Object reduce(JS5Function f, JS5Object initial);
 	
 	
 	/**
-	 * function reduceRight(f, initial)
-	 * 
-	 * @param {Function} f
-	 * @param {Object} initial
+	 * <b>function reduceRight(f, initial)</b> reduce an array from right-to-left
+	 * <h3>Example</h3>
+	 * <code>
+	 *  [2,10,60].reduceRight(function(x,y){return x/y;}); //returns 3 (60/10)/2<br>
+	 * </code> 
+	 * @param {Function} f A function that combines two values and returns a "reduced" value
+	 * @param {Object} <i>initial</i> An optional initial value to see the array reduction with. 
 	 * @returns {Object}
 	 * @memberOf Array
 	 * @see org.fife.rsta.ac.js.ecma.api.ecma5.JS5Array Array
+	 * @see #reduce(JS5Function, JS5Object) reduce()
 	 * @since Standard ECMA-262 5th. Edition
 	 */
 	public JS5Object reduceRight(JS5Function f, JS5Object initial);
 	
 	
 	/**
-	 * function some(predicate, o)
-	 * 
-	 * @param {Function} predicate
-	 * @param {Object} o
-	 * @returns {Boolean}
+	 * <b>function some(predicate, o)</b> test whether a predicate is true for any element.
+	 * <h3>Example</h3>
+	 * <code>
+	 *  [1,2,3].some(function(x){return x > 5;} //=>false<br>
+	 *  [1,2,3].some(function(x){return x > 2;} //=>true<br>
+	 * 	[].some(function(x){return true;} //=>false, always false for []<br>
+	 * </code> 
+	 * @param {Function} predicate A predicate function to test array elements.
+	 * @param {Object} <i>o</i> The optional <b><i>this</i></b> value for the invocations of  <b><i>predicate</i></b>.
+	 * @returns {Boolean} <b><i>true</i></b> if <b><i>predicate</i></b> returns <b><i>true</i></b> for at least one element of <b>array</b>, otherwise <b><i>false</i></b>
 	 * @memberOf Array
 	 * @see org.fife.rsta.ac.js.ecma.api.ecma5.JS5Array Array
 	 * @since Standard ECMA-262 5th. Edition
